a005aa183db0c6e03b8e0dffb3c21393ef7e27d7
JasinAliShananAntera <jasin@anterasoftware.com>
2023-10-19 22:43:30 +0200

    Login v2

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  username = '';
+  password = '';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    console.log({
+      email: this.username,
+      password: this.password,
+    });
+
+    this.http
+      .post('https://ac08-77-77-216-64.ngrok-free.app/api/owner-login', {
+        email: this.username,
+        password: this.password,
+      })
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
-
 }
